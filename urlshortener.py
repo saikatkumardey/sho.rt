d7e2426e8a12ad6caad71f390267d7d8d014e1b9
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2014-12-22 17:28:03 +0530

    initial commit of url_shortener project

---
+++ urlshortener.py
@@ -0,0 +1,87 @@
+#AUTHOR: Saikat Kumar Dey
+
+
+import string
+from random import Random,randint
+import sqlite3
+
+class Url_Shortener():
+	
+	def __init__(self):
+		self.char_set= string.ascii_letters + string.digits
+		self.base= len(self.char_set)
+		self.connection= sqlite3.connect('url_database')
+		self.db= self.connection.cursor()
+		self.db.execute("create table if not exists url_shortener(id integer primary key autoincrement,\
+			long_url varchar(1000),short_url varchar(30))")
+
+	def process_url(self,long_url):
+
+		index_of_slash= long_url.rfind('/')
+		domain= long_url[:index_of_slash]
+		id_url= long_url[index_of_slash+1:] #get the string after the '/' which is the string to be encoded
+
+		return index_of_slash,domain,id_url
+
+
+	def encode_url(self,long_url):
+
+		##check if the long_url is already present in the database, if yes the short_url corresponding to it
+		existing_row= self.db.execute("select * from url_shortener where long_url='%s'"%long_url).fetchone()
+		if existing_row!=None:
+			return existing_row[2]
+
+
+		index_of_slash,_,id_url= self.process_url(long_url)
+		domain="sho.rt"  #my_custom domain name
+		cursor= self.db.execute("select max(id) from url_shortener")
+		max_num= cursor.fetchone()[0]
+		if max_num==None:
+			max_num=1
+		else:
+			max_num+=1
+		
+		auto_increment_num= max_num
+		digits_base=[]
+		while auto_increment_num > 0:
+			remainder= auto_increment_num%self.base
+			digits_base.append(remainder)
+			auto_increment_num/=self.base
+
+		short_url= domain+'/'+''.join([self.char_set[i] for i in digits_base])
+
+		statement= "insert into url_shortener(long_url,short_url) values('%s','%s')"%(long_url,short_url)
+
+		self.db.execute(statement)
+
+		return short_url
+
+	def decode_url(self,encoded_string):
+
+		_,_,id_url= self.process_url(encoded_string)
+
+		i=0
+		orig_id=0
+		for character in id_url:
+			index_of_char= self.char_set.find(character)
+			orig_id+= pow(self.base,i)* index_of_char
+			i+=1
+		get_long_url= self.db.execute("select * from url_shortener where id=%d"%orig_id).fetchone()[1]
+		return get_long_url
+		#print "hey,",get_long_url.fetchone()[1]
+
+	def print_stuff(self):
+		print "THE DATABASE: ",self.db.execute("select * from url_shortener").fetchall()
+
+
+
+my_url= Url_Shortener()
+
+while(1):
+	try:
+		long_url= raw_input("Enter you long url: ")
+		short_url= my_url.encode_url(long_url)
+		print "Here's your short_url: ",short_url
+		print "decoding it again to test: ",my_url.decode_url(short_url)
+	except:
+		my_url.connection.commit()
