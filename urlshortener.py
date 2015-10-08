2b53b84a292570017757515aaf529ac270880b19
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2015-10-08 10:29:03 +0530

    added validation

--- urlshortener.py
+++ urlshortener.py
@@ -1,5 +1,6 @@
 import string
 from random import Random,randint
+from lepl.apps.rfc3696 import HttpUrl
 import sqlite3
 
 class Url_Shortener():
@@ -11,6 +12,7 @@ class Url_Shortener():
 		self.db= self.connection.cursor()
 		self.db.execute("create table if not exists url_shortener(id integer primary key autoincrement,\
 			long_url varchar(1000),short_url varchar(30))")
+		self.validator= HttpUrl()
 
 	def process_url(self,long_url):
 
@@ -22,15 +24,24 @@ class Url_Shortener():
 
 
 	def encode_url(self,long_url):
+		"""
+
+		returns the short url if the long url is valid, -1 if it's invalid
+
+		"""
+
+		##validate the url
+		if self.validator(long_url) == False:
+			print " Please enter a valid URL (http://sampleurl.com) "
+			return -1
+
 
 		##check if the long_url is already present in the database, if yes the short_url corresponding to it
 		existing_row= self.db.execute("select * from url_shortener where long_url='%s'"%long_url).fetchone()
 		if existing_row!=None:
 			return existing_row[2]
 
-
-		index_of_slash,_,id_url= self.process_url(long_url)
-		domain="sho.rt"  #my_custom domain name
+		domain="http://sho.rt"  #my_custom domain name
 		cursor= self.db.execute("select max(id) from url_shortener")
 		max_num= cursor.fetchone()[0]
 		if max_num==None:
@@ -44,6 +55,7 @@ class Url_Shortener():
 			remainder= auto_increment_num%self.base
 			digits_base.append(remainder)
 			auto_increment_num/=self.base
+		digits_base.reverse()
 
 		short_url= domain+'/'+''.join([self.char_set[i] for i in digits_base])
 
@@ -55,11 +67,17 @@ class Url_Shortener():
 
 	def decode_url(self,encoded_string):
 
+		"""
+
+		returns the original url
+
+		"""
+
 		_,_,id_url= self.process_url(encoded_string)
 
 		i=0
 		orig_id=0
-		for character in id_url:
+		for character in id_url[::-1]:
 			index_of_char= self.char_set.find(character)
 			orig_id+= pow(self.base,i)* index_of_char
 			i+=1
@@ -74,13 +92,17 @@ def main():
 	my_url= Url_Shortener()
 	while(1):
 		try:
-			long_url= raw_input("\n\nEnter you long url: ")
+			print "\n"
+			long_url= raw_input("Enter you long url: ")
 			short_url= my_url.encode_url(long_url)
-			print "\n\nHere's your short_url: ",short_url
-			print "\n\n"
+			if short_url == -1:
+				#the URL is invalid
+				continue
+			print "\nHere's your short_url: ",short_url
+			#print "\n\n"
 			print "decoding it again to test: ",my_url.decode_url(short_url)
 			my_url.connection.commit() #commit changes to the database
-			print "\n\n"
+			print "\n"
 		except:
 			break
 
